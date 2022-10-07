36e65ff9657800ca183de6fcda2703f5538778a6
Matt Seath <Seath.Dev@gmail.com>
2022-10-08 09:13:13 +1000

    migrate models

--- core/models.py
+++ core/models.py
@@ -1,31 +1,35 @@
-from enum import unique
 from django.db import models
 
+
 # Create your models here.
+class Sprite(models.Model):
+    image = models.ImageField()
+
+
 class Username(models.Model):
     name = models.CharField(max_length=255)
-    sprite = models.ImageField(null=True)
+    sprite = models.ForeignKey(Sprite, on_delete=models.PROTECT)
 
 
 class Battle(models.Model):
     title = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
     match_format = models.CharField(max_length=255)
-    player_1 = models.ForeignKey(Username, on_delete=models.PROTECT)
-    player_2 = models.ForeignKey(Username, on_delete=models.PROTECT)
-    victor = models.ForeignKey(Username, on_delete=models.PROTECT)
+    player_1 = models.ForeignKey(Username, on_delete=models.PROTECT, related_name="player_1")
+    player_2 = models.ForeignKey(Username, on_delete=models.PROTECT, related_name="player_2")
+    victor = models.ForeignKey(Username, on_delete=models.PROTECT, related_name="victor")
 
 
 class PokemonType(models.Model):
     title = models.CharField(max_length=255)
-    sprite = models.ImageField()
+    sprite = models.ForeignKey(Sprite, on_delete=models.PROTECT)
 
 
 class Pokemon(models.Model):
     name = models.CharField(max_length=255)
     dex_number = models.IntegerField()
-    type_1 = models.ForeignKey(PokemonType, on_delete=models.PROTECT)
-    type_2 = models.ForeignKey(PokemonType, null=True, on_delete=models.PROTECT)
-    sprite = models.ImageField()
+    type_1 = models.ForeignKey(PokemonType, on_delete=models.PROTECT, related_name="type_1")
+    type_2 = models.ForeignKey(PokemonType, null=True, on_delete=models.PROTECT, related_name="type_2")
+    sprite = models.ForeignKey(Sprite, on_delete=models.PROTECT)
     
 
