919a5e1d097b42e5153191eaa51d72c2e6b26b55
Matt Seath <Seath.Dev@gmail.com>
2022-10-08 08:10:52 +1000

    comments app added

--- core/models.py
+++ core/models.py
@@ -7,6 +7,10 @@ class Username(models.Model):
 
 class Battle(models.Model):
     title = models.CharField(max_length=255)
-    link = models.CharField(max_lenght=255)
-    player_1 = models.ForeignKey(Username, on_delete=models.PROTECT, related_name="battles")
-    player_2 = models.ForeignKey(Username, on_delete=models.PROTECT, related_name="battles")
+    link = models.CharField(max_length=255)
+    match_format = models.CharField(max_length=255)
+    player_1 = models.ForeignKey(Username, on_delete=models.PROTECT)
+    player_2 = models.ForeignKey(Username, on_delete=models.PROTECT)
+    victor = models.ForeignKey(Username, on_delete=models.PROTECT)
+
+
