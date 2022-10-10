07768f475e3888d7edca35752fc37ad24ca87cc2
Matt Seath <seath.dev@gmail.com>
2022-10-10 22:57:13 +1000

    added routers for react app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import HomePage from "./components/HomePage";
+// import TeamsPage from "./components/TeamsPage";
+// import BattlesPage from "./components/BattlesPage";
 
 function App() {
   return (
-    <div className="App">
-      <button className="btn btn-primary">I am button</button>
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/">
+          <p>This is the Homepage.</p>
+        </Route>
+        <Route path="/teams">
+          <p>This is the Teamspage</p>
+        </Route>
+        <Route path="/battles">
+          <p>This is the Battlespage</p>
+        </Route>
+      </div>
+    </Router>
   );
 }
 
