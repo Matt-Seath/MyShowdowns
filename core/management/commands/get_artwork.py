8e78a37ef700e90a3329797033dc62bb4448578d
Matt Seath <Seath.Dev@gmail.com>
2022-10-24 21:20:48 +1000

    optimise artwork command

--- core/management/commands/get_artwork.py
+++ core/management/commands/get_artwork.py
@@ -15,32 +15,33 @@ class Command(BaseCommand):
         artwork_path = os.path.join(settings.BASE_DIR, "media/artwork/pokemon/")
         Path(artwork_path).mkdir(parents=True, exist_ok=True)
 
+        """Get count and names of current Pokemon"""
+        url_request = "https://pokeapi.co/api/v2/pokemon/?limit=1000000"
+        response = requests.get(url_request)
+        if response.status_code == 200:
+            pokemon_list = response.json()
+            total_count = pokemon_list["count"]
 
-        """Download the artwork for each pokemon and store it in the created directory"""
-        for pokemon in range(5000):
-            pokemon = str(pokemon + 1)
-            print(pokemon)
+            for item in pokemon_list["results"]:
+                pokemon_response = requests.get(item["url"])
+                if pokemon_response.status_code == 200:
+                    pokemon = pokemon_response.json()
+                    print(pokemon["name"])
 
-            image = artwork_path + pokemon + ".png"
+                    """Download the artwork for each pokemon and store it in the created directory"""
+                    artwork_url = pokemon["sprites"]["other"]["official-artwork"]["front_default"]
+                    image = artwork_path + str(pokemon["id"]) + ".png"
+                    artwork_response = requests.get(artwork_url)
 
-            url_request = f"https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/official-artwork/{pokemon}.png"
-
-
-            file = open(image, "wb")
-
-            response = requests.get(url_request)
-            if response.status_code == 200:
-                file.write(requests.get(url_request).content)
-                file.close
-
-                print(image + " has successfully downloaded.")
-
-            elif response.status_code == 404:
-                print(f"{image} does not exist.")
-                break
-             
-            else:
-                print("Error. Could not retrieve" + image)
-
+                    if artwork_response.status_code == 200:
+                        file = open(image, "wb")
+                        file.write(artwork_response.content)
+                        file.close
+                        print(image + " has successfully downloaded.")
+                    elif response.status_code == 404:
+                        print(f"{image} does not exist.")                
+                    else:
+                        print("Error. Could not retrieve" + image)
+                    
 
         print("Done.")
