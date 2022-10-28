22aff05732e32b4a080819b61fae7d850de66ad2
Matt Seath <Seath.Dev@gmail.com>
2022-10-28 20:11:05 +1000

    updated models for admin page

--- core/models.py
+++ core/models.py
@@ -5,9 +5,9 @@ from django.db import models
 # Create your models here.
 class Art(models.Model):
     id = models.IntegerField(primary_key=True)
-    artwork = models.ImageField(null=True)
-    front = models.ImageField(null=True)
-    front_shiny = models.ImageField(null=True)
+    artwork = models.ImageField(null=True, blank=True)
+    front = models.ImageField(null=True, blank=True)
+    front_shiny = models.ImageField(null=True, blank=True)
 
     def __str__(self) -> str:
         return str(self.id)
@@ -37,7 +37,7 @@ class Battle(models.Model):
     player_1 = models.ForeignKey(Username, on_delete=models.PROTECT, related_name="player_1")
     player_2 = models.ForeignKey(Username, on_delete=models.PROTECT, related_name="player_2")
     victor = models.ForeignKey(Username, on_delete=models.PROTECT, related_name="victor")
-    description = models.CharField(max_length=255, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
     date_created = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
@@ -58,10 +58,10 @@ class BasePokemon(models.Model):
 
     name = models.CharField(max_length=255)
     type_1 = models.CharField(max_length=255, choices=TYPE_LIST)
-    type_2 = models.CharField(max_length=255, null=True, choices=TYPE_LIST)
+    type_2 = models.CharField(max_length=255, blank=True, null=True, choices=TYPE_LIST)
     ability_1 = models.ForeignKey(Ability, on_delete=models.PROTECT, related_name="ability_1")
-    ability_2 = models.ForeignKey(Ability, null=True, on_delete=models.PROTECT, related_name="ability_2")
-    ability_3 = models.ForeignKey(Ability, null=True, on_delete=models.PROTECT, related_name="ability_3")
+    ability_2 = models.ForeignKey(Ability, null=True, blank=True, on_delete=models.PROTECT, related_name="ability_2")
+    ability_3 = models.ForeignKey(Ability, null=True, blank=True, on_delete=models.PROTECT, related_name="ability_3")
     artwork = models.ForeignKey(Art, on_delete=models.CASCADE)
     base_hp = models.IntegerField()
     base_att = models.IntegerField()
@@ -84,7 +84,7 @@ class CustomPokemon(models.Model):
     ]
 
     pokemon = models.ForeignKey(BasePokemon, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=55, null=True)
+    nickname = models.CharField(max_length=55, blank=True, null=True)
     nature = models.CharField(choices=NATURE_LIST, max_length=255, null=True)
     ability = models.ForeignKey(Ability, on_delete=models.PROTECT)
     ivs_hp = models.IntegerField(default=31)
