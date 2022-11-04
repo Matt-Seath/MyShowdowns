230292691c615c3b92e7849d2c211e778f7e73b3
Matt Seath <seath.dev@gmail.com>
2022-11-04 22:34:17 +1000

    updated serializers

--- core/serializers.py
+++ core/serializers.py
@@ -10,11 +10,31 @@ class UsernameSerializer(serializers.ModelSerializer):
 class BattleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Battle
-        fields = ["id", "title", "link", "match_format", "player_1", "player_2", "victor"]
+        fields = [
+            "id", "title", "link", "match_format", "gametype", 
+            "player_1", "player_2", "victor", "description",
+            "p1_pk_1", "p1_pk_2", "p1_pk_3", "p1_pk_4", "p1_pk_5",
+            "p1_pk_6", "p2_pk_1", "p2_pk_2", "p2_pk_3", "p2_pk_4",
+            "p2_pk_5", "p2_pk_6"
+        ]
 
 
 class BasePokemonSerializer(serializers.ModelSerializer):
     class Meta:
         model = BasePokemon
-        fields = ["id", "name", "dex_number", "type_1", "type_2"]
+        fields = [
+            "id", "name", "type_1", "type_2", "ability_1", "ability_2",
+            "ability_3", "artwork", "base_hp", "base_att", "base_def",
+            "base_sp_att", "base_sp_def", "base_spd", "tier"
+        ]
 
+
+class CustomPokemonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomPokemon
+        fields = [
+            "id", "pokemon", "nickname", "nature", "ability", 
+            "ivs_hp", "ivs_att", "ivs_def", "ivs_sp_att", "ivs_sp_def",
+            "ivs_spd", "evs_hp", "evs_att", "evs_def", "evs_sp_att", 
+            "evs_sp_def", "evs_spd"
+        ]
