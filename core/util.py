85be618693f0865cf3788fa2c933dcd41663b152
Matt Seath <seath.dev@gmail.com>
2022-11-07 18:06:44 +1000

    added util.py for parse_showdown function

---
+++ core/util.py
@@ -0,0 +1,92 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def parse_showdown_link(url, description=None):
+    TEXT_STRINGS = [
+        "|player|p1|", "|player|p2|",
+        "|tier|", "|gametype|",
+        "|win|",
+    ]
+
+    TEXT_PKM = [
+        "|poke|p1|", "|poke|p2|",
+    ]
+
+    BATTLE_VARS = [
+        "player_1", "player_2",
+        "match_format", "gametype",
+        "victor",
+    ]
+
+    BATTLE_PKM = [[
+        "p1_pk_1", "p1_pk_2",
+        "p1_pk_3", "p1_pk_4",
+        "p1_pk_5", "p1_pk_6",
+        ], [
+        "p2_pk_1", "p2_pk_2",
+        "p2_pk_3", "p2_pk_4",
+        "p2_pk_5", "p2_pk_6",
+    ]]
+
+    battle_dict = {}
+
+    def find_value(string, text, ignore_commas=False):
+        start_index = text.find(string)
+        start_index += len(string)
+        end_index = text.find("|", start_index)
+        end_line = text.find("\n", start_index)
+        if end_index > end_line:
+            end_index = end_line
+        if not ignore_commas:
+            end_comma = text.find(",", start_index)
+            if end_index > end_comma and end_comma != -1:
+                end_index = end_comma
+        string = text[start_index:end_index]
+
+        return string
+
+
+    def find_all(sub, a_str):
+        start = 0
+        while True:
+            start = a_str.find(sub, start)
+            if start == -1: return
+            string_end = start + len(sub)
+            end_index = a_str.find("|", string_end)
+            end_comma = a_str.find(",", string_end)
+            if end_index > end_comma and end_comma != -1:
+                end_index = end_comma
+            yield text[string_end:end_index].lower()
+            start += len(sub)
+ 
+
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
+    script = doc.find_all("script")
+    if script:
+        default_description = doc.find_all("meta")[1].get("content")
+        text = script[1].get_text()
+        battle_dict["title"] = doc.title.string
+        if description:
+            battle_dict["description"] = description
+        else:
+            battle_dict["description"] = default_description
+        battle_dict["link"] = url
+
+        for i in range(len(TEXT_STRINGS)):
+            value = find_value(TEXT_STRINGS[i], text)
+            if value:
+                battle_dict[BATTLE_VARS[i]] = value
+            else:
+                print(f"Error! Could not find {TEXT_STRINGS[i]}")
+                return 1
+        for i in range(2):
+            for j, value in enumerate(find_all(TEXT_PKM[i], text)):
+                battle_dict[BATTLE_PKM[i][j]] = value
+
+        print(battle_dict)
+    else:
+        print("not found")
+
+    return battle_dict
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import (
     CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin)
+from .util import parse_showdown_link
 from .models import *
 from .serializers import *
 
@@ -9,6 +11,15 @@ from .serializers import *
 class BattleViewSet(ModelViewSet):
     queryset = Battle.objects.all()
     serializer_class = BattleSerializer
+
+    def create(self, request, *args, **kwargs):
+        battle_dict = parse_showdown_link(request.request.link)
+        print(battle_dict)
+        serializer = BattleSerializer(data=battle_dict)
+        serializer.is_valid(raise_exception=True)
+        battle = serializer.save()
+        serializer = BattleSerializer(battle)
+        return Response(serializer.data)
 
 
 class PokemonViewSet(ModelViewSet):
